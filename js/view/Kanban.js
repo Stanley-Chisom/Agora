3d3f50ecb005a2ae6593e30f0df3a64ea7fc60ab
Stanley-Chisom <stanley.chisom042@gmail.com>
2022-10-24 12:24:55 +0100

    initial and final commit 😁

---
+++ js/view/Kanban.js
@@ -0,0 +1,34 @@
+import Column from "./Column.js";
+
+export default class Kanban {
+	constructor(root) {
+		this.root = root;
+
+		Kanban.columns().forEach(column => {
+			const columnView = new Column(column.id, column.title);
+
+			this.root.appendChild(columnView.elements.root);
+		});
+	}
+
+	static columns() {
+		return [
+			{
+				id: 1,
+				title: "Not Started 🚀"
+			},
+			{
+				id: 2,
+				title: "In Progress 🟨"
+			},
+			{
+				id: 3,
+				title: "Completed ✅"
+			},
+				{
+				id: 4,
+				title: "Cancelled ❌"
+			}
+		];
+	}
+}
